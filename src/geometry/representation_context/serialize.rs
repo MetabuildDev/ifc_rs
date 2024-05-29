e617f5ac162139eea940865e338e4c9464360bd6
Gerasimos Kougianos <gerasimos.kougianos@metabuild.io>
2024-05-29 11:49:38 +0200

    fix geometric representation context serialization

--- src/geometry/representation_context/serialize.rs
+++ src/geometry/representation_context/serialize.rs
@@ -6,7 +6,7 @@ impl Display for GeometricRepresentationContext {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "IFCGEOMETRICREPRESENTATIONCONTEXT(#{context_id}, {context_type}, {coord_dims}, {precision}, {world_coord_system}, {true_north});",
+            "IFCGEOMETRICREPRESENTATIONCONTEXT({context_id},{context_type},{coord_dims},{precision},{world_coord_system},{true_north});",
             context_id = self.context_identifier,
             context_type = self.context_type,
             coord_dims = self.coord_space_dimension,
