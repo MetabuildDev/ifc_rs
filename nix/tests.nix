876c68259573f11a7e0791069a27041d5812c687
robert.walter <robert.walter@metabuild.io>
2024-06-24 10:43:06 +0000

    cleanup deps (#136)
    Reviewed-on: https://git.mtbld.de/MetabuildStudio/IFC4/pulls/136

--- nix/tests.nix
+++ nix/tests.nix
@@ -8,18 +8,33 @@
       ...
     }:
     {
-      packages.exec-tests = pkgs.writeShellApplication {
-        name = "run-tests";
-        runtimeInputs = [
-          self'.packages.rust
-          pkgs.cargo-nextest
-          pkgs.clang
-        ];
-        text = ''
-          cargo nextest --version
-          cargo nextest run --release
-          echo SUCCESS
-        '';
+      packages = {
+        exec-tests = pkgs.writeShellApplication {
+          name = "run-tests";
+          runtimeInputs = [
+            self'.packages.rust
+            pkgs.cargo-nextest
+            pkgs.clang
+          ];
+          text = ''
+            cargo nextest --version
+            cargo nextest run --release
+            echo SUCCESS
+          '';
+        };
+        exec-deps-check = pkgs.writeShellApplication {
+          name = "check-deps";
+          runtimeInputs = [
+            self'.packages.rust
+            pkgs.cargo-machete
+            pkgs.clang
+          ];
+          text = ''
+            cargo machete --version
+            cargo machete
+            echo SUCCESS
+          '';
+        };
       };
     };
 }
