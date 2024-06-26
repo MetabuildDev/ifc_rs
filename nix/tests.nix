13462e97de91dd3da8da1005e2a0a78495795ed0
robert.walter <robert.walter@metabuild.io>
2024-06-26 10:06:30 +0000

    Add doc coverage CI check (#146)
    Reviewed-on: https://git.mtbld.de/MetabuildStudio/IFC4/pulls/146

--- nix/tests.nix
+++ nix/tests.nix
@@ -4,6 +4,7 @@
     {
       self',
       pkgs,
+      lib,
       system,
       ...
     }:
@@ -39,8 +40,11 @@
           name = "check-docs";
           runtimeInputs = [
             self'.packages.rust-nightly
+
+            pkgs.git
             pkgs.ripgrep
             pkgs.clang
+            pkgs.bc
           ];
           text = ''
             coverage=$(RUSTDOCFLAGS='-Z unstable-options --show-coverage' cargo doc 2>&1 | rg "Documenting ifc_rs " -A 9999)
@@ -48,6 +52,32 @@
             echo "$coverage" | rg "100.0%"
             echo Needs further documentation: 
             echo "$coverage" | rg --invert-match "100.0%"
+
+            branch=$(git rev-parse --abbrev-ref HEAD)
+            base=$(git merge-base HEAD origin/main)
+            echo "branch: $branch"
+
+            echo "checkout base"
+            git checkout "$base"
+            echo "calculate percentage before"
+            before=$(RUSTDOCFLAGS='-Z unstable-options --show-coverage' cargo doc 2>&1 | rg "Documenting ifc_rs " -A 9999 | rg Total | cut -d '|' --fields=4 | xargs)
+            echo "checkout branch"
+            git checkout "$branch"
+            echo "calculate percentage after"
+            after=$(RUSTDOCFLAGS='-Z unstable-options --show-coverage' cargo doc 2>&1 | rg "Documenting ifc_rs " -A 9999 | rg Total | cut -d '|' --fields=4 | xargs)
+
+            echo "before: $before, after: $after"
+
+            a=$(echo "$after" | awk '{print $1/100}')
+            b=$(echo "$before" | awk '{print $1/100}')
+
+            if echo "$a>=$b" | bc -l; then
+              echo NO DOCUMENTATION REGRESSIONS
+              exit 0
+            else 
+              echo REGRESSIONS, PLEASE CHECK EVALUATION ABOVE
+              exit 1
+            fi
           '';
         };
       };
